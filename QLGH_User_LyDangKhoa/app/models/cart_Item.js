fedd46c461484282edcc15953a82da044d712a0f
truongduytan200115-commits <truongduytan20015@gmail.com>
2026-02-13 13:29:52 +0700

    upload source

--- QLGH_User_LyDangKhoa/app/models/cart_Item.js
+++
@@ -1,6 +0,0 @@
-export default class CartItem {
-    constructor(_product, _quantity) {
-        this.product = _product; // Đây là object sản phẩm (chứa id, name, price...)
-        this.quantity = _quantity; // Số lượng mua
-    }
-}
